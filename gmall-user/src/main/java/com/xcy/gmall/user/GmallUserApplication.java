971138fb40c0c1d4e017ff544c2a9b1d13014de2
jerry <13717736@qq.com>
2018-12-29 17:38:22 +0800

    002

---
+++ gmall-user/src/main/java/com/xcy/gmall/user/GmallUserApplication.java
@@ -0,0 +1,16 @@
+package com.xcy.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.xcy.gmall.user.mapper")
+public class GmallUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallUserApplication.class, args);
+	}
+
+}
+
